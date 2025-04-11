35d013c7180aa8d1c29ac4ed561e9aca75b8c649
Graves Bi <805401869@qq.com>
2025-04-11 23:46:57 +1000

    Add files via upload
    Update dockerfile and requirements

--- Dockerfile
+++ Dockerfile
@@ -1,17 +1,13 @@
-# 使用 Python 官方镜像
+# userservice/Dockerfile
 FROM python:3.11
 
-# 设置工作目录
 WORKDIR /app
 
-# 复制项目文件到容器
-COPY . /app
+COPY requirements.txt .
 
-# 安装依赖
+RUN pip install --upgrade pip setuptools wheel
 RUN pip install --no-cache-dir -r requirements.txt
 
-# 设置环境变量，防止 Python 缓存影响容器
-ENV PYTHONUNBUFFERED=1
+COPY . .
 
-# 运行 Flask
-CMD ["python", "app.py"]
+CMD ["python", "run.py"]
--- run.py
+++ run.py
@@ -1,8 +1,12 @@
+from flask_jwt_extended import JWTManager
 from app import create_app
 from app.nacos.client import start_nacos_registration
 
 app = create_app()
+app.config["JWT_SECRET_KEY"] = "here-is-the-secret"  # 修改为你自己的密钥
+app.config["JWT_VERIFY_SUB"]=False
+jwt = JWTManager(app)
 
 if __name__ == "__main__":
     start_nacos_registration()
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    app.run(host="0.0.0.0", port=7000, debug=True)
