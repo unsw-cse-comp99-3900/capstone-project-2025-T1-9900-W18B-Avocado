1bf1dada19ac353d29c5fcbdc3b4e090bcae1279
Graves Bi <805401869@qq.com>
2025-04-11 23:59:06 +1000

    Add files via upload
    Updated db login information

--- app/models/db.py
+++ app/models/db.py
@@ -4,8 +4,8 @@ from app.nacos.client import get_mysql_config
 mysql_config = get_mysql_config()
 
 DB_CONFIG = {
-    "host": mysql_config.get("host", "localhost"),
-    "user": mysql_config.get("user", "root"),
+    "host": mysql_config.get("host", "mysql"),
+    "user": mysql_config.get("user", "user"),
     "password": mysql_config.get("password", "123456"),
     "database": mysql_config.get("database", "demo_db")
 }
